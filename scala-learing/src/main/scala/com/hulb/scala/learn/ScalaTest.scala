140c02528fe1488564ccb660c1fdfe3348b0b892
lxh <xuehuan.lxh@dtwave-inc.com>
2018-06-21 15:06:38 +0800

    add streaming update spark to 2.3.1

--- scala-learing/src/main/scala/com/hulb/scala/learn/ScalaTest.scala
+++ scala-learing/src/main/scala/com/hulb/scala/learn/ScalaTest.scala
@@ -1,5 +1,7 @@
 package com.hulb.scala.learn
 
+import scala.io.Source
+
 object ScalaTest {
   def main(args: Array[String]): Unit = {
     println(1)
@@ -8,6 +10,13 @@ object ScalaTest {
       * 符号字面量
       */
     println('ddd)
+
+    val source = Source.fromFile("/Users/hulb/data/removedata")
+    val param = source.mkString
+    println(param.length)
+
   }
 
 }
+
+
