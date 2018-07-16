1794343910d2d5c5d0f68b91cd5acde4488b959a
lxh <xuehuan.lxh@dtwave-inc.com>
2018-07-16 14:37:04 +0800

    init manager offset

--- spark-learning/spark-sql/src/main/scala/com/hulb/spark/streaming/kafka/StreamingFromKafka.scala
+++ spark-learning/spark-sql/src/main/scala/com/hulb/spark/streaming/kafka/StreamingFromKafka.scala
@@ -22,6 +22,11 @@ import kafka.api.PartitionOffsetRequestInfo
 import kafka.consumer.SimpleConsumer
 import kafka.api.TopicMetadataRequest
 
+
+/**
+  * 在初始化 kafka stream 的时候，查看 zookeeper 中是否保存有 offset，有就从该 offset 进行读取，没有就从最新/旧进行读取。
+  * 在消费 kafka 数据的同时，将每个 partition 的 offset 保存到 zookeeper 中进行备份
+  */
 object StreamingFromKafka {
   val groupId = "logs"
   val topic = "streaming"
@@ -57,22 +62,36 @@ object StreamingFromKafka {
     sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
 
+    //kafka参数
     val kafkaParams = Map("metadata.broker.list" -> "localhost:9092",
       "group.id" -> groupId,
       "zookeeper.connect" -> "localhost:9999",
       "auto.offset.reset" -> kafka.api.OffsetRequest.SmallestTimeString)
 
+    //创建 stream 时使用的 topic 名字集合
     val topics = Set(topic)
+    //查询该路径下是否子节点（默认有子节点为我们自己保存不同 partition 时生成的）
     val children = zkClient.countChildren(s"${topicDirs.consumerOffsetDir}")
     var kafkaStream: InputDStream[(String, String)] = null
+    //如果 zookeeper 中有保存 offset，我们会利用这个 offset 作为 kafkaStream 的起始位置
     var fromOffsets: Map[TopicAndPartition, Long] = Map()
 
     if (children > 0) {
       //---get partition leader begin----
+      // 如果 kafka 上的 offset 已经过期，那么就会报 OffsetOutOfRange 的异常，因为之前保存在 zk 的 offset 已经 topic 中找不到了。
+      // 所以我们需要在 从 zk 找到 offset 的这种情况下增加一个判断条件，如果 zk 中保存的 offset 小于当前 kafka topic 中最小的 offset，则设置为 kafka topic 中最小的 offset。
+      // 从 kafka上获取offset的时候，需要寻找对应的leader，从leader来获取 offset.
+      // 以下为获取不同 partition 的 leader 相关代码
       val topicList = List(topic)
-      val req = new TopicMetadataRequest(topicList, 0) //得到该topic的一些信息，比如broker,partition分布情况
-      val getLeaderConsumer = new SimpleConsumer("localhost", 9092, 10000, 10000, "OffsetLookup") // brokerList的host 、brokerList的port、过期时间、过期时间
-      val res = getLeaderConsumer.send(req) //TopicMetadataRequest   topic broker partition 的一些信息
+
+      //得到该topic的一些信息，比如broker,partition分布情况
+      val req = new TopicMetadataRequest(topicList, 0)
+
+      // brokerList的host 、brokerList的port、过期时间、过期时间
+      val getLeaderConsumer = new SimpleConsumer("localhost", 9092, 10000, 10000, "OffsetLookup")
+
+      //TopicMetadataRequest   topic broker partition 的一些信息
+      val res = getLeaderConsumer.send(req)
       val topicMetaOption = res.topicsMetadata.headOption
       val partitions = topicMetaOption match {
         case Some(tm) =>
@@ -80,27 +99,37 @@ object StreamingFromKafka {
         case None =>
           Map[Int, String]()
       }
+
+      //如果保存过 offset，还应该和 kafka 上最小的 offset 做对比，不然会报 OutOfRange 的错误
       for (i <- 0 until children) {
         val partitionOffset = zkClient.readData[String](s"${topicDirs.consumerOffsetDir}/${i}")
         val tp = TopicAndPartition(topic, i)
         //---additional begin-----
         val requestMin = OffsetRequest(Map(tp -> PartitionOffsetRequestInfo(OffsetRequest.EarliestTime, 1))) // -2,1
+
+        //最小offset
         val consumerMin = new SimpleConsumer(partitions(i), 9092, 10000, 10000, "getMinOffset")
         val curOffsets = consumerMin.getOffsetsBefore(requestMin).partitionErrorAndOffsets(tp).offsets
         var nextOffset = partitionOffset.toLong
-        if (curOffsets.length > 0 && nextOffset < curOffsets.head) { //如果下一个offset小于当前的offset
+        if (curOffsets.length > 0 && nextOffset < curOffsets.head) {
+          //如果下一个offset小于当前的最小offset，从最小offset开始消费
           nextOffset = curOffsets.head
         }
         //---additional end-----
+        //将不同 partition 对应的 offset 增加到 fromOffsets 中
         fromOffsets += (tp -> nextOffset)
-        fromOffsets += (tp -> partitionOffset.toLong) //将不同 partition 对应的 offset 增加到 fromOffsets 中
+        // TODO 是否可以不需要这一行。fromOffsets += (tp -> partitionOffset.toLong) //将不同 partition 对应的 offset 增加到 fromOffsets 中
       }
+      //这个会将 kafka 的消息进行 transform，最终 kafak 的数据都会变成 (topic_name, message) 这样的 tuple
       val messageHandler = (mmd: MessageAndMetadata[String, String]) => (mmd.topic, mmd.message()) //这个会将 kafka 的消息进行 transform，最终 kafak 的数据都会变成 (topic_name, message) 这样的 tuple
       kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)](ssc, kafkaParams, fromOffsets, messageHandler)
 
     } else {
+      //如果未保存，根据 kafkaParam 的配置使用最新或者最旧的 offset
       kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
     }
+
+    //得到该 rdd 对应 kafka 的消息的 offset
     var offsetRanges = Array[OffsetRange]()
     kafkaStream.transform { rdd =>
       offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
@@ -112,6 +141,7 @@ object StreamingFromKafka {
         }
         }
         for (o <- offsetRanges) {
+          //处理,消费完后, 将该 partition 的 offset 保存到 zookeeper
           ZkUtils.updatePersistentPath(zkClient, s"${topicDirs.consumerOffsetDir}/${o.partition}", o.fromOffset.toString)
         }
       }
